a7f48029786926b11b5a13dabb4b840c30a9f54d
aleksey-berezin <aleksey@berez.in>
2026-01-24 20:16:07 -0800

    Remove unused animation code and fix CloudCannon preview
    - Remove animate-on-scroll.js and all animation-related code
    - Remove animate-on-scroll wrapper from base.html
    - Clean up CSS references to animate-on-scroll
    - Add output_url to CloudCannon config for preview screenshots
    - Simplify JavaScript bundle (removed 2.3KB of unused code)

--- _source/assets/js/app.js
+++ _source/assets/js/app.js
@@ -1,10 +1,8 @@
 /* ----------------------------------------------------------------------------
 js entry point
 ---------------------------------------------------------------------------- */
-import './_elements/animate-on-scroll.js';
 import './_elements/contact-form.js';
 import './_elements/count-up.js';
 import './_elements/burger-menu.js';
 import './_elements/header-metrics.js';
 import './_elements/side-nav-scroll.js';
-import './_elements/scroll-animations.js';
