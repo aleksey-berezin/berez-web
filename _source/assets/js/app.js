0b3518897ab4a0898bff57ef65d0bf0462dd7e73
aleksey-berezin <aleksey@berez.in>
2026-01-24 21:15:04 -0800

    Simplify OG image to static homepage screenshot
    - Remove dynamic hash fragment support for OG screenshots
    - Use simple static screenshot of homepage for all pages
    - Cleaner, more reliable approach

--- _source/assets/js/app.js
+++ _source/assets/js/app.js
@@ -6,4 +6,3 @@ import './_elements/count-up.js';
 import './_elements/burger-menu.js';
 import './_elements/header-metrics.js';
 import './_elements/side-nav-scroll.js';
-import './_elements/dynamic-og-screenshot.js';
